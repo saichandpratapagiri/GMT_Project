44056b41229ead980850e64e98d367cca595f4d1
saichandpratapagiri <saichand.pratapagiri@gmail.com>
2020-04-10 17:32:57 +0530

    1. Turned app orientation to portrait. 2. Small changed to login ui.

--- app/src/main/java/com/example/gmtandroid/LoginActivity.java
+++ app/src/main/java/com/example/gmtandroid/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.gmtandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+
+    public void toRegistraction(View view) {
     }
 }
