eb9f8cd082a0cd783e564dd0c17cdc9b1aae857c
saichandpratapagiri <saichand.pratapagiri@gmail.com>
2020-04-12 16:32:09 +0530

    1.FingerPrint is enabled. 2. Added bottom navigation bar

---
+++ app/src/main/java/com/example/gmtandroid/Login/LoginActivity.java
@@ -0,0 +1,245 @@
+package com.example.gmtandroid.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gmtandroid.R;
+import com.example.gmtandroid.Utilities.FingerprintHandler;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEt, pswdEt;
+    private String username, pswd;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+    private String KEY_NAME = "my_key";
+    private Cipher cipher;
+    private KeyguardManager keyguardManager;
+    private FingerprintManager fingerprintManager;
+    private FingerprintManager.CryptoObject cryptoObject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        usernameEt = findViewById(R.id.usernameEt);
+        pswdEt = findViewById(R.id.pswdEt);
+
+        if(checkLockScreen()){
+            generateKey();
+            if (cipherInit()) {
+                FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                FingerprintHandler helper = new FingerprintHandler(this);
+                helper.startAuth(fingerprintManager, cryptoObject);
+            }
+        }
+
+        usernameEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().equals(" ")){
+                    usernameEt.setText("");
+                }
+            }
+        });
+
+        pswdEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().equals(" ")){
+                    pswdEt.setText("");
+                }
+            }
+        });
+
+        pswdEt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_GO){
+                    validateAndNavigate();
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private Boolean checkLockScreen() {
+         keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+         fingerprintManager = (FingerprintManager) getSystemService(Context.FINGERPRINT_SERVICE);
+
+        if (!keyguardManager.isKeyguardSecure()) {
+
+            Toast.makeText(this,
+                    "Lock screen security not enabled",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,
+                    "Permission not enabled (Fingerprint)",
+                    Toast.LENGTH_LONG).show();
+
+            return false;
+        }
+
+        if (!fingerprintManager.hasEnrolledFingerprints()) {
+            Toast.makeText(this,
+                    "No fingerprint registered, please register",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+
+    }
+
+    private void generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES,
+                    "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean cipherInit(){
+        try {
+            cipher = Cipher.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES + "/"
+                            + KeyProperties.BLOCK_MODE_CBC + "/"
+                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME, null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return  false;
+    }
+
+
+
+    private void validateAndNavigate() {
+        username = usernameEt.getText().toString();
+        pswd = pswdEt.getText().toString();
+        if(username != null && !username.equals("")){
+            if(pswd != null && !pswd.equals("") ){
+                finish();
+                startActivity(new Intent(LoginActivity.this, PostLogin.class));
+            }
+            else {
+                pswdEt.setError("Password cannot be nil");
+            }
+        }else {
+            usernameEt.setError("Username cannot be nil");
+        }
+    }
+
+    public void toRegistraction(View view) {
+    }
+}
